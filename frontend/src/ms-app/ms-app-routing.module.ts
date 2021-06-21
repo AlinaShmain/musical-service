2909f6733593083cdc36c3cd8939bca04ec00b5d
ALina Ulyanova <ulyanova.alina1997@gmail.com>
2021-06-21 19:57:00 +0300

    add form route

--- frontend/src/ms-app/ms-app-routing.module.ts
+++ frontend/src/ms-app/ms-app-routing.module.ts
@@ -4,18 +4,26 @@ import { MainComponent } from "./main/main.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { SignInComponent } from "./auth-modal/sign-in/sign-in.component";
 import { SignUpComponent } from "./auth-modal/sign-up/sign-up.component";
-import { AuthModalGuard } from "./guards/auth-modal-guard";
+// import { AuthModalGuard } from "./guards/auth-modal-guard";
 import { ArtistListComponent } from "./artist-list/artist-list.component";
 import { AlbumListComponent } from "./album-list/album-list.component";
 import { FavouriteListComponent } from "./favourite-list/favourite-list.component";
 import { PlaylistListComponent } from "./playlist-list/playlist-list.component";
 import { HomeComponent } from "./home/home.component";
+import { ModalEntryComponent } from "./modalEntry/modal-entry";
 
 const routes: Routes = [
     { path: "", pathMatch: "full", redirectTo: "/main/home" },
-    { path: "main", component: MainComponent,
+    {
+        path: "main", component: MainComponent,
         children: [
             { path: "", pathMatch: "full", redirectTo: "home" },
+            {
+                path: "form",
+                component: ModalEntryComponent,
+                // redirectTo: "/home/form(popupContent:signIn)",
+                // pathMatch: "full",
+            },
             { path: "home", component: HomeComponent },
             { path: "artists", component: ArtistListComponent },
             { path: "albums", component: AlbumListComponent },
@@ -23,8 +31,22 @@ const routes: Routes = [
             { path: "playlists", component: PlaylistListComponent },
         ]
     },
-    { path: "signIn", component: SignInComponent, canActivate: [AuthModalGuard] },
-    { path: "signUp", component: SignUpComponent, canActivate: [AuthModalGuard] },
+    {
+        path: "home",
+        component: HomeComponent,
+        children: [
+            {
+                path: "form",
+                component: ModalEntryComponent,
+                // redirectTo: "/home/form(popupContent:signIn)",
+                // pathMatch: "full",
+            },
+        ]
+    },
+    { path: "signIn", outlet: "popupContent", component: SignInComponent },
+    { path: "signUp", outlet: "popupContent", component: SignUpComponent },
+    // { path: "signIn", component: SignInComponent, canActivate: [AuthModalGuard] },
+    // { path: "signUp", component: SignUpComponent, canActivate: [AuthModalGuard] },
     { path: "page-not-found", component: PageNotFoundComponent },
     { path: "**", redirectTo: "/page-not-found" },
 ];
